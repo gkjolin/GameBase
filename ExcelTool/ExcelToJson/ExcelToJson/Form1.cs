2b5b38bc8e7be3841794cd04b13ea4f0be47cdb1
zhaoyabo <240572111@qq.com>
2015-08-27 10:50:26 +0800

    excel to json

---
+++ ExcelTool/ExcelToJson/ExcelToJson/Form1.cs
@@ -0,0 +1,89 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LitJson;
+
+namespace ExcelTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Console.WriteLine("start build ");
+            return;
+            ExcelToJson();
+        }
+
+        private void ConvertExcel(string fullPath, string fileName)
+        {
+            FileStream fs = File.Open(fullPath, FileMode.Open, FileAccess.Read);
+            IExcelDataReader dataReader = ExcelReaderFactory.CreateOpenXmlReader(fs);
+            DataSet ds = dataReader.AsDataSet();
+            int row = ds.Tables[0].Rows.Count;
+            JsonData jd_root = new JsonData();
+            jd_root["name"] = fileName.Substring(0, fileName.Length - 5);
+            for (int i = 2; i < row; i++)
+            {
+                JsonData sub_jd = new JsonData();
+                var obj = ds.Tables[0].Rows[i];
+                object[] arr_objs = obj.ItemArray;
+                for (int j = 0; j < arr_objs.Length; j++)
+                {
+                    object value = arr_objs[j];
+                    string key = ds.Tables[0].Rows[0][j].ToString();
+                    string type = ds.Tables[0].Rows[1][j].ToString();
+
+                    if (type == "int")
+                    {
+                        sub_jd[key] = Convert.ToInt32(value);
+                    }
+                    else if (type == "string")
+                    {
+                        sub_jd[key] = Convert.ToString(value);
+                    }
+                }
+                jd_root[ds.Tables[0].Rows[i][0].ToString()] = sub_jd;
+            }
+
+            WriteToJson(fileName, JsonMapper.ToJson(jd_root));
+
+        }
+
+        private void WriteToJson(string fileName, string json)
+        {
+            string fn = fileName.Replace("xlsx", "json");
+            if (Directory.Exists("D://tables//json/") == false)
+            {
+                Directory.CreateDirectory("D://tables//json/");
+            }
+            string path = "D://tables//json/" + fn;
+            FileInfo fi = new FileInfo(path);
+            StreamWriter sw = fi.CreateText();
+            sw.Write(json);
+            sw.Close();
+            sw.Dispose();
+        }
+
+        private void ExcelToJson()
+        {
+            string directory_url = "D:\\tables\\";
+            DirectoryInfo _directoryInfo = new DirectoryInfo(directory_url);
+            FileInfo[] arrFiles = _directoryInfo.GetFiles("*.xlsx", SearchOption.TopDirectoryOnly);
+            for (int l = 0; l < arrFiles.Length; l++)
+            {
+                Console.WriteLine("file name : " + arrFiles[l].Name);
+                ConvertExcel(arrFiles[l].FullName, arrFiles[l].Name);
+            }
+        }
+
+    }
+}
---
+++ ExcelTool/ExcelTool/ExcelTool/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ExcelTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_des = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_des
+            // 
+            this.lbl_des.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_des.AutoSize = true;
+            this.lbl_des.Font = new System.Drawing.Font("SimSun", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbl_des.Location = new System.Drawing.Point(143, 62);
+            this.lbl_des.Name = "lbl_des";
+            this.lbl_des.Size = new System.Drawing.Size(110, 27);
+            this.lbl_des.TabIndex = 0;
+            this.lbl_des.Text = "lbl_dec";
+            this.lbl_des.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 161);
+            this.Controls.Add(this.lbl_des);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_des;
+    }
+}
+
