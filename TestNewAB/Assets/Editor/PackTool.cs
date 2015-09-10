d6e219bbabb35d53e429c46c54d1a565988f9c1a
zhaoyabo <240572111@qq.com>
2015-09-10 13:28:49 +0800

    test ab commit

---
+++ TestNewAB/Assets/Editor/PackTool.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+
+public class PackTool : MonoBehaviour
+{
+    [MenuItem("PackTool/Pack")]
+    public static void Pack()
+    {
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath);
+    }
+
+    //test
+    [MenuItem("PackTool/SetFileBundleName")]
+    static void SetBundleName()
+    {
+        //PrintArray(AssetDatabase.GetSubFolders("Assets"));
+        //PrintArray(AssetDatabase.GetDependencies(new string[]{"Assets/StreamingAssets/uilogin"}));
+        //Path.get
+        //Selection.
+        //AssetDatabase.SetLabels()
+        Object o = AssetDatabase.LoadMainAssetAtPath("Assets/c");
+        string[] s = new string[] { "generat" };
+        AssetDatabase.SetLabels(o, s);
+        AssetImporter.GetAtPath("Assets/c").assetBundleName = "c";
+
+        return;
+        #region 设置资源的AssetBundle的名称和文件扩展名
+        UnityEngine.Object[] selects = Selection.objects;
+
+        Debug.Log("Selection.activeGameObject.name :" + Selection.activeGameObject.name);
+        Debug.Log("Selection.activeObject.name :" + Selection.activeObject.name);
+        Debug.Log("Selection.activeObject.name :" + Selection.gameObjects.Length);
+
+        Debug.Log("selects length " + selects.Length);
+        foreach (UnityEngine.Object selected in selects)
+        {
+            string path = AssetDatabase.GetAssetPath(selected);
+            Debug.Log(path);
+            AssetImporter asset = AssetImporter.GetAtPath(path);
+            asset.assetBundleName = selected.name; //设置Bundle文件的名称
+            //asset.assetBundleVariant = "unity3d";//设置Bundle文件的扩展
+            asset.SaveAndReimport();
+
+            //AssetDatabase.
+        }
+        AssetDatabase.Refresh();
+        #endregion
+    }
+    /// <summary>
+    /// 以文件夹形式，将一个文件里的资源 打到 一个 assetbundle里，assetbundle name 为文件夹的名字
+    /// </summary>
+    [MenuItem("PackTool/PackAssetBundleByFolder")]
+    public static void PackAssetBundleByFolder()
+    {
+        AssetDatabase.RemoveUnusedAssetBundleNames();
+        string[] arrFolders = AssetDatabase.GetSubFolders("Assets/AssetBundle");
+        for (int i = 0; i < arrFolders.Length; i++)
+        {
+            string folderName = arrFolders[i];
+            AssetImporter ai = AssetImporter.GetAtPath(folderName);
+            DirectoryInfo di = new DirectoryInfo(Application.dataPath + folderName);
+            ai.assetBundleName = di.Name;
+        }
+    }
+
+    /// <summary>
+    /// ui界面 打成单个 assetbundle
+    /// </summary>
+    [MenuItem("PackTool/PackAssetBundleSingle")]
+    public static void PackAssetBundleSingleOfUIPrefab()
+    {
+        DirectoryInfo di = new DirectoryInfo(Application.dataPath + "/Resources/prefabs/UIPrefab");
+        FileInfo[] files = di.GetFiles("*.prefab");
+        for (int i = 0; i < files.Length; i++)
+        {
+            FileInfo fi = files[i];
+            string p = "Assets/Resources/prefabs/UIPrefab/" + fi.Name;
+            Debug.Log(p + "      fileName " + fi.Name);
+            AssetImporter ai = AssetImporter.GetAtPath(p);
+            ai.assetBundleName = fi.Name.Substring(0,fi.Name.Length-7);
+        }
+        AssetDatabase.Refresh();
+    }
+
+    public static void PrintArray(string[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Debug.Log(" print array index " + i + "  " + arr[i]);
+        }
+    }
+}
---
+++ TestNewAB/Assets/Scripts/TestLoadImage.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class TestLoadImage : MonoBehaviour
+{
+
+    private Transform mTransform;
+    void Start()
+    {
+        mTransform = this.transform;
+        //StartCoroutine(LoadUIAB());
+        //StartCoroutine(LoadUI("uilogin"));
+        StartCoroutine(LoadManifest());
+    }
+    private AssetBundleManifest manifest;
+    private IEnumerator LoadManifest()
+    {
+        string path = "file://" + Application.streamingAssetsPath + "/StreamingAssets";
+        WWW www = new WWW(path);
+        yield return www;
+        manifest = www.assetBundle.LoadAsset("AssetBundleManifest") as AssetBundleManifest;
+        www.assetBundle.Unload(false);
+        StartCoroutine(LoadAB("uilogin", (ab) =>
+        {
+            GameObject go = Instantiate(ab.LoadAsset("uilogin") as GameObject);
+            go.transform.SetParent(mTransform);
+            go.transform.localScale = Vector3.one;
+            go.transform.localPosition = Vector3.zero;
+        }));
+    }
+
+    private string[] GetAllDependecies(string name)
+    {
+        return manifest.GetAllDependencies(name);
+    }
+
+    private AssetBundle uiAB;
+    private IEnumerator LoadUIAB()
+    {
+        string path = "file://" + Application.streamingAssetsPath + "/ui";
+        WWW www = new WWW(path);
+        yield return www;
+        www.assetBundle.LoadAsset("ui");
+        StartCoroutine(LoadUI("uilogin"));
+    }
+
+    private IEnumerator LoadAB(string name, Action<AssetBundle> callBack = null)
+    {
+        string[] arrDepends = GetAllDependecies(name);
+        if (arrDepends != null && arrDepends.Length > 0)
+        {
+            for (int i = 0; i < arrDepends.Length; i++)
+            {
+                string pathD = "file://" + Application.streamingAssetsPath + "/" + arrDepends[i];
+                WWW wwwD = new WWW(pathD);
+                yield return wwwD;
+                wwwD.assetBundle.LoadAsset(arrDepends[i]);
+                if(i == arrDepends.Length-1)
+                {
+                    StartCoroutine(LoadUI("uilogin"));
+                }
+            }
+        }
+    }
+
+    private GameObject CreatImage(Sprite s)
+    {
+        GameObject go = new GameObject();
+        Image image = go.AddComponent<Image>();
+        image.sprite = s;
+        go.transform.SetParent(mTransform);
+        go.transform.localPosition = Vector3.zero;
+        go.transform.localScale = Vector3.one;
+        go.name = s.name;
+        return go;
+    }
+
+    private IEnumerator LoadUI(string name)
+    {
+        string path = "file://" + Application.streamingAssetsPath + "/" + name;
+        WWW www = new WWW(path);
+        yield return www;
+        GameObject go = Instantiate(www.assetBundle.LoadAsset(name) as GameObject);
+        go.transform.SetParent(mTransform);
+        go.transform.localScale = Vector3.one;
+        go.transform.localPosition = Vector3.zero;
+        www.assetBundle.Unload(false);
+    }
+
+}
