7d1776055f17a90cc40a2f5c53a68a024b05f94a
zhaoyabo <240572111@qq.com>
2015-10-12 10:44:23 +0800

    ssssssssssss

--- GameBase/Assets/Scripts/Enity/EventComponent.cs
+++ GameBase/Assets/Scripts/Enity/EventComponent.cs
@@ -8,9 +8,7 @@ namespace Game
     public delegate void EventHandle();
     public class EventComponent : BaseComponent
     {
-        //private Dictionary<string, List<Action>> _dic = new Dictionary<string, List<Action>>();
         private Dictionary<string, EventHandle> _dict = new Dictionary<string, EventHandle>();
-
 
         public void AddEventListener(string key, EventHandle handle)
         {
@@ -60,43 +58,6 @@ namespace Game
                 _dict[key]();
             }
         }
-
-        /*public void DispatcherEvent(string _key)
-        {
-            if (_dic.ContainsKey(_key))
-            {
-                List<Action> _list = _dic[_key];
-                for (int i = 0; i < _list.Count; i++)
-                {
-                    _list[0]();
-                }
-            }
-        }
-
-        public void AddEventListener(string _key, Action _action)
-        {
-            List<Action> _list;
-            if (_dic.ContainsKey(_key))
-            {
-                _list = _dic[_key];
-            }
-            else
-            {
-                _list = new List<Action>();
-                _dic.Add(_key, _list);
-            }
-            _list.Add(_action);
-        }
-
-        public void RemoveEventListener(string _key, Action _action)
-        {
-            if (_dic.ContainsKey(_key))
-            {
-                List<Action> _list = _dic[_key];
-                if (_list.Contains(_action))
-                    _list.Remove(_action);
-            }
-        }*/
 
     }
 }
